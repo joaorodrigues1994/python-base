9d6d9ea9299a8a49733163143360520865507e21
joaorodrigues1994 <jpaulo.rodrigues@hotmail.com>
2026-04-29 01:02:40 -0300

    Finalizacao do dia

--- hello.py
+++ hello.py
@@ -1,3 +1,23 @@
 #!/usr/bin/env python3
-# Este comentário será ignorado. Programa Hello World.
-print("Hello, World!")
+
+"""
+Hello World Multi Linguas.
+
+Dependendo da lingua configurada no ambiente, o programa exibe a mensagem correspondente.
+
+Como usar:
+
+Tenha a variavel LANG devidamente configurada. Ex:
+  export LANG=pt_BR
+Execução:
+  python3 hello.py
+  ou
+  ./hello.py
+"""
+__version__ = "0.0.1"
+__author__ = "Joao Paulo"
+__licence__ = "OpenSource"
+
+
+msg = "Hello, World!"
+print(msg)
